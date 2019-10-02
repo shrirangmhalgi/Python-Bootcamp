7288663233a129b59de1b4f92258afca0ec1b489
Shrirang Mhalgi <shrirangmhalgi@gmail.com>
2019-10-03 01:02:50 +0530

    explored mathematical operations in python 3

---
+++ 6. Numbers and Floats/math_operation.py
@@ -0,0 +1,13 @@
+print(type(9))
+print(type(9.0)) # prints the type of the data 
+
+# PEMDAS (Parenthesis, Exponential, Multiplication, Division, Addition, Substraction) is order of evaluating a python expression
+
+# exponents in python
+print(2 ** 3)
+
+# modulo
+print(10 % 3)
+
+# integer division
+print(10 // 2)
