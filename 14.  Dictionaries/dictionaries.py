f93d1125b31b11c3b62954bc38059936515110f7
Shrirang Mhalgi <shrirangmhalgi@gmail.com>
2019-10-13 21:27:52 +0530

    explored dictionaries in python

---
+++ 14.  Dictionaries/dictionaries.py
@@ -0,0 +1,13 @@
+# keys can be numbers or strings
+# values can be anything
+sample_dictionary = {
+    "first_name" : "shrirang",
+    "middle_name" : "rajendra",
+    "last_name" : "mhalgi"
+}
+
+print(sample_dictionary)
+
+sample_dictionary2 = dict(first_name = "gauri", middle_name = "rajendra", last_name = "mhalgi")
+
+print(sample_dictionary2)
