e05be0e17772abe4fbaf1f8294e9ee5592a2ef62
Shrirang Mhalgi <shrirangmhalgi@gmail.com>
2019-10-26 01:43:02 +0530

    Explored debugging in python

--- 21. Debugging and error handling/debugging.py
+++ 21. Debugging and error handling/debugging.py
@@ -1,2 +1,15 @@
-values = list(range(1, 11))
-print(*values)
+import pdb
+
+first = "first"
+second = "second"
+pdb.set_trace()
+result = first + second
+third = "third"
+result += third
+print(result)
+
+# common pdb commands
+# l -> list
+# n -> next line
+# p -> print
+# c -> continue / finishes debugging
