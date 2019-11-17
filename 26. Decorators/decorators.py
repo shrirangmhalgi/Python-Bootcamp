7b3e26d50d5233398f79ea8430258d5c0b7769fa
Shrirang Mhalgi <shrirangmhalgi@gmail.com>
2019-11-17 22:55:36 +0530

    Explored decorators in python

--- 26. Decorators/decorators.py
+++ 26. Decorators/decorators.py
@@ -1,1 +1,39 @@
-# decorators
+from random import choice
+
+# funcs as args to other funcs
+def sum(n, func):
+	total = 0
+	for num in range(1,n+1):
+		total += func(num)
+	return total
+
+def square(x):
+	return x*x
+
+def cube(x):
+	return x*x*x
+
+print(sum(3,cube))
+print(sum(3,square))
+
+# return funcs from other funcs
+def make_laugh_func():
+    def get_laugh():
+        l = choice(('HAHAHAH', 'lol', 'tehehe'))
+        return l
+
+    return get_laugh
+
+laugh = make_laugh_func()
+print(laugh())
+
+# nesting functions inside one another
+def greet(person):
+	def get_mood():
+		msg = choice(('Hello there ', 'Go away ', 'I love you '))
+		return msg
+
+	result = get_mood() + person
+	return result
+
+print(greet("Toby"))
