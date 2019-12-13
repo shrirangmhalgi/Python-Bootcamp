677521727c5334f1d87bac094e1e99f2577a9b0e
Shrirang Mhalgi <shrirangmhalgi@gmail.com>
2019-12-13 15:20:20 +0530

    Created a quote guessing game in python

--- 31. Web Scraping/scraping_game.py
+++ 31. Web Scraping/scraping_game.py
@@ -3,6 +3,7 @@
 import requests
 from bs4 import BeautifulSoup
 from time import sleep
+from random import choice
 
 all_quotes = []
 base_url = "http://quotes.toscrape.com"
@@ -22,6 +23,31 @@ while url:
 
     next_btn = soup.find(class_="next")
     url = next_btn.find("a")["href"] if next_btn else None
-    sleep(2)
+    # sleep(2)
 
-print(all_quotes)
+quote = choice(all_quotes)
+remaining_guesses = 4
+guess = ""
+
+print(f"Here's a quote : {quote['text']}")
+while guess.lower() != quote["author"].lower() and remaining_guesses > 0:
+    guess = input(f"Who said this quote? Guesses remaining : {remaining_guesses}\n")
+    
+    if guess.lower() == quote["author"].lower():
+        break
+
+    remaining_guesses -= 1
+    if remaining_guesses == 3:
+        response = requests.get(f"{base_url}{quote['bio-link']}")
+        soup = BeautifulSoup(response.text, "html.parser")
+        birth_date = soup.find(class_="author-born-date").get_text()
+        birth_place = soup.find(class_="author-born-location").get_text()
+        print(f"Here's a hint: The author was born {birth_place} on {birth_date}")
+    elif remaining_guesses == 2:
+        print(f"Here's a hint: The author's name starts with letter {quote['author'][0]}")
+    elif remaining_guesses == 1:
+        last_name_initials = quote['author'].split(" ")[1][0]
+        print(f"Here's a hint: The author's last name starts with letter {last_name_initials}")
+    else:
+        print(f"Sorry... You ran out of guesses... The answer was {quote['author']}")
+        
